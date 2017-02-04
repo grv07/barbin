a93ccb9d6f962de8d88b448a38402c9d9fe6e1a6
gaurav <you@example.com>
2017-02-04 23:02:26 +0530

    add a custom styles to img tag

--- src/app/create-blog/create-blog.component.ts
+++ src/app/create-blog/create-blog.component.ts
@@ -11,19 +11,21 @@ import { Component, OnInit } from '@angular/core';
 
 export class CreateBlogComponent{
 
-  public editorContent = `<p>My HTML</p>`;
+  public editorContent = `
+     <blockquote>Q. How to style my text ?</blockquote><blockquote>A. <strong>Select Tex</strong>t and <u>click</u> on <span style="color: rgb(230, 0, 0);">function</span> <em>buttons</em> on <span style="background-color: rgb(255, 153, 0);">header</span>.</blockquote><p>* Color text style not working ( We are working on this editor to improve stuff on this.)</p><blockquote>Q. Why style stick on my cursor ?</blockquote><blockquote>A. Please click again on function button related to the style you want to remove.</blockquote><p></p><blockquote>Q. How to write code ?</blockquote><blockquote>A. Write select and click on button on menu.</blockquote>
+  `;
   public editorConfig = {
-    placeholder: "输入公告内容，支持html"
+    placeholder: "Type something ..."
   };
 
   constructor() {}
   
   onEditorCreated(quill) {
-    // console.log('this is quill object', quill);
+    console.log('this is quill object', quill);
   }
   
   onContentChanged({ quill, html, text }) {
-    console.log(text, this.editorContent.indexOf('img'));
+    console.log(text, html);
   }
 
 }
